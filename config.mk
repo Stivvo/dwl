88380e1bd1ca057bc2af9005e2bc200d347f145e
Stivvo <stivvo01@gmail.com>
2020-10-04 23:39:13 +0200

    enable xwayland

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@
 CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-error=unused-function
 
 # Uncomment to build XWayland support
-#CFLAGS += -DXWAYLAND
+CFLAGS += -DXWAYLAND
