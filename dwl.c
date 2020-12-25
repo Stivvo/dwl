3e32d34f5df9d632c0ecb8f33654868e8d66b00a
Stivvo <stivvo01@gmail.com>
2020-12-25 15:52:52 +0100

    Render the focused fscrn client or everything else
    Adapt to Monitor.focused
    
    This follows the philosophy of the recent changes in the fullscreen
    branch. Render only the fullscreen window when is focused, otherwise
    keep rendering everything as usual. This allows more flexibility because
    other clients can still be used even if one client is fullscreen, or
    even multiple fullscreen client can exist in the same tag.

--- dwl.c
+++ dwl.c
@@ -1600,9 +1600,9 @@ renderclients(Monitor *m, struct timespec *now)
 		/* Only render visible clients which show on this monitor */
 		if (!VISIBLEON(c, c->mon) || !wlr_output_layout_intersects(
 					output_layout, m->wlr_output, &c->geom) ||
-				(m->fullscreenclient && m->fullscreenclient != c))
-				/* Alternative: render at least all fullscreen windows*/
-				/* (m->fullscreenclient && !c->isfullscreen)) */
+				(m->focus->isfullscreen && c != m->focus))
+				/* render only the focused fullscreen client if there is one in
+				 * that monitor */
 			continue;
 
 		surface = WLR_SURFACE(c);
@@ -1691,7 +1691,7 @@ rendermon(struct wl_listener *listener, void *data)
 		wlr_renderer_clear(drw, rootcolor);
 
 		renderlayer(&m->layers[ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND], &now);
-		if (!m->fullscreenclient)
+		if (!m->focus || !m->focus->isfullscreen)
 			renderlayer(&m->layers[ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM], &now);
 		renderclients(m, &now);
 #ifdef XWAYLAND
